ba53e40504d616be31e75d0976fe2a890775703e
dexter68555 <46341564+dexter68555@users.noreply.github.com>
2022-09-02 21:55:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Web3 = require('web3');
 
-const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/b8362868f4464226b4f84c38c9ae8752'));
+const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/<YOUR_PROJECT_ID>'));
 
 const balanceOfABI = [
     {
@@ -24,13 +24,17 @@ const balanceOfABI = [
     },
 ];
 
+//this is DAI token contract
+//Please change it to the preferred token address
 const tokenContract = "0x6b175474e89094c44da98b954eedeac495271d0f";
+
+//This is wallet address
+//Please change to the preferred wallet address
 const walletAddress = ["0xf326e4de8f66a0bdc0970b79e0924e33c79f1915"];
 
 const contract = new web3.eth.Contract(balanceOfABI, tokenContract)
 
-
-
+//This is the getTokenBalance function that will get the token balance of a wallet
 async function getTokenBalance() {
 	
 	for(var i = 0; i < walletAddress.length;i++){
